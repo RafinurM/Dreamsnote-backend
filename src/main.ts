88f43a7a294324a6bb75ddd819ed3e51e7402012
Rafinur Mursalimov <ArtArtistRM@yandex.ru>
2025-08-27 14:03:29 +0300

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://dreamsnote.ru/' });
+  app.enableCors({
+    origin: [
+      'https://dreamsnote.ru',
+      'https://www.dreamsnote.ru',
+      'http://localhost:5173',
+    ],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
